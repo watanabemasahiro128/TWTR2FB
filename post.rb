818deeed363cc0b0ff83f9d6cad1be9aee746b4f
Masahiro <watanabemasahiro128@gmail.com>
2022-05-14 20:27:37 +0900

    Change To Notify Sentry

--- post.rb
+++ post.rb
@@ -3,6 +3,7 @@
 require 'json'
 require 'dotenv'
 require 'selenium-webdriver'
+require 'sentry-ruby'
 require_relative 'lib/twitter'
 require_relative 'lib/facebook'
 
@@ -11,35 +12,46 @@ TWITTER_BEARER_TOKEN = ENV['TWITTER_BEARER_TOKEN']
 TWITTER_USER_ID = ENV['TWITTER_USER_ID']
 FACEBOOK_EMAIL = ENV['FACEBOOK_EMAIL']
 FACEBOOK_PASSWORD = ENV['FACEBOOK_PASSWORD']
+SENTRY_DSN = ENV['SENTRY_DSN']
 
-twitter_client = Twitter::Client.new(twitter_bearer_token: TWITTER_BEARER_TOKEN)
-tweets = twitter_client.tweets(twitter_user_id: TWITTER_USER_ID).slice(0, 10)
-if File.exist?("#{__dir__}/posted_tweet_ids.json")
-  posted_tweet_ids = File.open("#{__dir__}/posted_tweet_ids.json", 'r') { |file| JSON.parse(file.read)['ids'] }
-  tweets.reject! { |tweet| posted_tweet_ids.include?(tweet[:id]) }
+Sentry.init do |config|
+  config.dsn = SENTRY_DSN
+  config.traces_sample_rate = 1.0
 end
 
-return if tweets.empty?
+begin
+  twitter_client = Twitter::Client.new(twitter_bearer_token: TWITTER_BEARER_TOKEN)
+  tweets = twitter_client.tweets(twitter_user_id: TWITTER_USER_ID).slice(0, 10)
+  if File.exist?("#{__dir__}/posted_tweet_ids.json")
+    posted_tweet_ids = File.open("#{__dir__}/posted_tweet_ids.json", 'r') { |file| JSON.parse(file.read)['ids'] }
+    tweets.reject! { |tweet| posted_tweet_ids.include?(tweet[:id]) }
+  end
 
-capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-  'goog:chromeOptions' => {
-    'args' => [
-      'headless',
-      'disable-gpu',
-      'lang=ja-JP',
-      'user-agent=Mozilla/5.0 (X11; CrOS aarch64 13597.84.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.106 Safari/537.36',
-      "user-data-dir=#{__dir__}/user_data"
-    ]
-  }
-)
-driver = Selenium::WebDriver.for(:chrome, capabilities:)
-driver.manage.timeouts.implicit_wait = 10
+  return if tweets.empty?
 
-facebook_client = Facebook::Client.new(email: FACEBOOK_EMAIL, password: FACEBOOK_PASSWORD)
-driver = facebook_client.login(driver:)
-tweets.each { |tweet| driver = facebook_client.post(driver:, message: tweet[:text]) }
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    'goog:chromeOptions' => {
+      'args' => [
+        'headless',
+        'disable-gpu',
+        'lang=ja-JP',
+        'user-agent=Mozilla/5.0 (X11; CrOS aarch64 13597.84.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.106 Safari/537.36',
+        "user-data-dir=#{__dir__}/user_data"
+      ]
+    }
+  )
+  driver = Selenium::WebDriver.for(:chrome, capabilities:)
+  driver.manage.timeouts.implicit_wait = 10
 
-posted_tweet_ids += tweets.map { |tweet| tweet[:id] }
-File.open("#{__dir__}/posted_tweet_ids.json", 'w') { |file| JSON.dump({ ids: posted_tweet_ids }, file) }
+  facebook_client = Facebook::Client.new(email: FACEBOOK_EMAIL, password: FACEBOOK_PASSWORD)
+  driver = facebook_client.login(driver:)
+  tweets.each { |tweet| driver = facebook_client.post(driver:, message: tweet[:text]) }
 
-driver.quit
+  posted_tweet_ids += tweets.map { |tweet| tweet[:id] }
+  File.open("#{__dir__}/posted_tweet_ids.json", 'w') { |file| JSON.dump({ ids: posted_tweet_ids }, file) }
+
+  driver.quit
+  Sentry.capture_message('Success', level: :info)
+rescue StandardError => e
+  Sentry.capture_exception(e)
+end
